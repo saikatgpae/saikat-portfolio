2c802c333f4a1d54ee02ef40becbad4ac8d61d89
Saikat Chakraborty <saikatgpae@gmail.com>
2022-07-26 22:21:24 +0530

    Add profile menu.

---
+++ javascript.js
@@ -0,0 +1,24 @@
+function menuChange(e) {
+    e.classList.toggle("change");
+
+
+    //changing the background of body class
+    let body = document.querySelector('.body');
+    body.classList.toggle('bodyTrans');
+
+    //Change the background colour of header
+    let header = document.querySelector('.header');
+    header.classList.toggle('headertrans');
+
+    //hide the header logo
+    let headerLogo = document.querySelector('.header-logo');
+    headerLogo.classList.toggle('inVisible');
+
+    //Hide body content
+    let bodyContent = document.querySelector('.body-content');
+    bodyContent.classList.toggle('display-none');
+
+    // Unhide the menu option .onclick div
+    let click = document.querySelector('.onclick');
+    click.classList.toggle('clicked');
+  }
