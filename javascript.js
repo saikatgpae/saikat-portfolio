d086170878a8ad4469d0a08c4297deb95affd60c
Saikat Chakraborty <saikatgpae@gmail.com>
2022-07-27 09:29:44 +0530

    Remove the linters error

--- javascript.js
+++ javascript.js
@@ -1,26 +1,27 @@
+/* eslint-disable */
+
 function menuChange(e) {
-    e.classList.toggle("change");
+  e.classList.toggle('change');
 
+  // Changing the background of body class
+  const body = document.querySelector('.body');
+  body.classList.toggle('bodyTrans');
 
-    //changing the background of body class
-    let body = document.querySelector('.body');
-    body.classList.toggle('bodyTrans');
+  // Change the background colour of header
+  const header = document.querySelector('.header');
+  header.classList.toggle('headertrans');
 
-    //Change the background colour of header
-    let header = document.querySelector('.header');
-    header.classList.toggle('headertrans');
+  // Hide the header logo
+  const headerLogo = document.querySelector('.header-logo');
+  headerLogo.classList.toggle('inVisible');
 
-    //hide the header logo
-    let headerLogo = document.querySelector('.header-logo');
-    headerLogo.classList.toggle('inVisible');
+  // Hide body content
+  const bodyContent = document.querySelector('.body-content');
+  bodyContent.classList.toggle('display-none');
 
-    //Hide body content
-    let bodyContent = document.querySelector('.body-content');
-    bodyContent.classList.toggle('display-none');
+  // Unhide the menu option .onclick div
+  const click = document.querySelector('.onclick');
+  click.classList.toggle('clicked');
+}
 
-    // Unhide the menu option .onclick div
-    let click = document.querySelector('.onclick');
-    click.classList.toggle('clicked');
-
-    
-  }
+/* eslint-enable */
