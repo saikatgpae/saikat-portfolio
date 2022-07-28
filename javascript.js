2a8368676746a5528d3329d31f20fa97c9ac1d34
Saikat Chakraborty <saikatgpae@gmail.com>
2022-07-28 13:10:05 +0530

    Add project pop up menu.

--- javascript.js
+++ javascript.js
@@ -32,7 +32,7 @@ function menuChange(e) {
 
 var mq = window.matchMedia( "(max-width: 768px)" );
 function seeProject(){
-  //Fix th #p1 id project page
+  
   if(mq.matches){
     //fix the pop up window
     const body = document.querySelector('#p1');
@@ -54,6 +54,33 @@ function seeProject(){
     //show the cross sighn
     document.querySelector('.cross').style.display = 'block';
   }
+
+  else{
+
+    //fix the pop up window
+    const body = document.querySelector('#p1');
+    body.classList.add('fixed','project_span_desktop');
+
+    //Add the project connten para
+    let project1 = document.querySelector('.project1-para');
+    project1.innerHTML = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi provident illum ab explicabo quasi officiis quas doloribus vero perferendis, repellat error temporibus eum minima recusandae corrupti, pariatur quibusdam voluptates tempora odio, dicta deleniti quos molestias! Corporis, libero voluptates. Amet, ea.";
+    //show the cross sighn
+    document.querySelector('.cross').style.display = 'block';
+
+    //change the .project-info display to flex
+    let prjInfo = document.querySelector('.project-info');
+    prjInfo.classList.add('project-info-desktop');
+    
+    //Hide the See Prject button & Show the live, source button
+    document.querySelector('.see-project-button').style.display = 'none';
+    document.querySelector('.see-live-button').style.display = 'block';
+    document.querySelector('.see-source-button').style.display = 'block';
+    
+
+    document.querySelector('#grb1','#grb2','#grb3','#grb4').style.display = 'block';
+
+  }
+
   
 
   
